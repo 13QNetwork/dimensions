3303f201d7069ad9c4180c208d6a864d1ae7397a
Adrian <adrianfcasares@gmail.com>
2021-09-06 14:40:01 -0500

    🎉  Initial commit

---
+++ src/main/java/dev/adrwas/dimensions/Dimensions.java
@@ -0,0 +1,14 @@
+package dev.adrwas.dimensions;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Dimensions implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+
+        System.out.println("🎉 Dimensions enabled!");
+
+    }
+
+}
